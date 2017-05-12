f507a51e694257c70d38d32663fd38b85a4dd5fc
group09 <s9ctmeye@stud.uni-saarland.de>
2017-05-12 20:42:29 +0200

    initial commit of the project

---
+++ businessTripApplication/src/dev/simpleServlets/helloServlet.java
@@ -0,0 +1,44 @@
+package dev.simpleServlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class helloServlet
+ */
+@WebServlet("/helloServlet")
+public class helloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public helloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().print("What's hatnin?");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ businessTripApplication/src/dev/simpleServlets/simpleServlet.java
@@ -0,0 +1,5 @@
+package dev.simpleServlets;
+
+public class simpleServlet {
+	
+}
